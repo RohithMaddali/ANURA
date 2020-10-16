0c401fd85b920fcdf3c81a04ac869a4ba6001ad4
Kyan Rowse <rowse9@gmail.com>
2020-10-16 20:27:49 +1100

    Added FMOD Plugin

---
+++ ANURA/Assets/Scripts/FMOD/F_Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
