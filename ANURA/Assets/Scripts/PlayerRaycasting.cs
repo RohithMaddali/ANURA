5cab99bdf047b882058626346eb392d450267f04
Luke Stanley <1015748@student.sae.edu.au>
2020-10-28 11:18:47 +1100

    Added gates to PRDE1
    Also made distance to touch 3 meters

--- ANURA/Assets/Scripts/PlayerRaycasting.cs
+++ ANURA/Assets/Scripts/PlayerRaycasting.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerRaycasting : MonoBehaviour
 {
-    public float distancetoTouch;
+    public float distancetoTouch = 3;
     RaycastHit touchingObject;
     
     // Start is called before the first frame update
