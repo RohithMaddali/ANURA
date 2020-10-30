26c89bb0cec021f615e94a42582e52aa9cee939d
Damien Harwood <1019243@student.sae.edu.au>
2020-10-30 14:07:29 +1100

    Added files and folders for map generation
    Added folder for testing
    Added scene with map manager
    Added script

---
+++ ANURA/Assets/Scenes/MapGen(Damien)/MapGenManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapGenerator
+{
+
+    public class MapGenManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+    }
+}
