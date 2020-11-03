5e86ad565f33ac76105bd9d8acaf69a2e077530a
Jacob <1014906@student.sae.edu.au>
2020-11-04 10:55:17 +1100

    Movement between points
    Basic AI for movement

---
+++ ANURA/Assets/Scripts/Ignore/randpatrol.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randpatrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform moveSpot;
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ;
+    public float maxY;
+    public float minY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+
+        moveSpot.position = new Vector3(Random.Range(minX, maxX), Random.Range(minZ, maxZ), Random.Range(minY, maxY)); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpot.position = new Vector3(Random.Range(minX, maxX), Random.Range(minZ, maxZ));
+                waitTime = startWaitTime;
+            }
+
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ ANURA/Assets/Scripts/Patroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int currentPoint;
+    public Transform[] patrolPoints;
+    public float moveSpeed;
+    
+    void Start()
+    {
+        transform.position = patrolPoints[0].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position, moveSpeed * Time.deltaTime);
+
+        if (transform.position == patrolPoints[currentPoint].position)
+        {
+            currentPoint = (currentPoint + 1) % patrolPoints.Length;
+        }
+    }
+}
