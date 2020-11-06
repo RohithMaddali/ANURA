913b5794e24cf65e510f80b7dd779148f291fba3
Kyan Rowse <rowse9@gmail.com>
2020-11-06 15:12:38 +1100

    Footsteps

---
+++ ANURA/Assets/Scripts/FMOD/F_Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class F_Player : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+    [SerializeField]
+    private float walkingSpeed = 1;
+    [SerializeField]
+    private float walkingBackwardsSpeed = 1;
+    
+    private void Start()
+    {
+        InvokeRepeating("WalkingFootsteps",0,walkingSpeed);
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+    void WalkingFootsteps()
+    {
+        if (playerMovement.isMoving == true)
+        {
+            RuntimeManager.PlayOneShot("event:/Player/P_Footsteps",default);
+        }
+    }
+}
