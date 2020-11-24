07ec1355755286b26eb6d94da50b5809b64c5a18
Pyro <onetruejake@outlook.com>
2020-11-24 12:58:11 +1100

    Made pause menu stop the player's head bobbing

--- ANURA/Assets/Scripts/PauseMenu.cs
+++ ANURA/Assets/Scripts/PauseMenu.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //serialize field makes private gameobjects viewable in editor
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+
+    private GameObject cammie;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //check for the esc key
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            cammie = GameObject.FindGameObjectWithTag("MainCamera");
+            cammie.GetComponent<HeadBobber>().bobbingSpeed = 0;
+            ActivateMenu();
+        }
+
+        else
+        {
+            Time.timeScale = 1;
+            cammie = GameObject.FindGameObjectWithTag("MainCamera");
+            cammie.GetComponent<HeadBobber>().bobbingSpeed = 0.07f;
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        pauseMenuUI.SetActive(true);
+    }
+
+    void DeactivateMenu()
+    {
+        pauseMenuUI.SetActive(false);
     }
 }
