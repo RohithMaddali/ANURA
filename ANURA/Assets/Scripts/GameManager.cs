09c1975a9896a8f51d6ffd10d1e3dcd64d93dc13
Luke Stanley <1015748@student.sae.edu.au>
2020-11-25 16:03:17 +1100

    Cursor reappears on death

--- ANURA/Assets/Scripts/GameManager.cs
+++ ANURA/Assets/Scripts/GameManager.cs
@@ -14,8 +14,6 @@ public class GameManager : MonoBehaviour
 
     public void Caught(GameObject toad, GameObject room)
     {
-        Debug.Log("turning on cursor");
-        Cursor.lockState = CursorLockMode.None;
         Debug.Log("changing scene");
         SceneManager.LoadScene(1);
     }
