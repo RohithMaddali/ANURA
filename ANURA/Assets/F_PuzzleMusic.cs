7a61ef6e21e6658ec0ce0e5b4779357a9cf98e88
Kyan Rowse <rowse9@gmail.com>
2020-11-27 17:34:03 +1100

    Auto stash before merge of "main" and "origin/main"

---
+++ ANURA/Assets/F_PuzzleMusic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class F_PuzzleMusic : MonoBehaviour
+{
+    private bool played;
+       private void OnTriggerEnter(Collider other)
+       {
+              if (other.gameObject.CompareTag("Player1") && played == false)
+              {
+                    RuntimeManager.PlayOneShot("event:/Music/PuzzleRoom", default);
+                    played = true;
+              }
+       }
+}
