aa32e30be2ac5069bc693288013b5200baf68728
Pyro <onetruejake@outlook.com>
2020-11-28 18:01:43 +1100

    added a small script for later

---
+++ ANURA/Assets/Scripts/Menuscripts/KeyTip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyTip : MonoBehaviour
+{
+    //set my gamemanager
+    public GameObject gamemanger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
