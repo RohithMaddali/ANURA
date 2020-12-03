5a99ac0f403e6ca590e411843b80bdb804541d83
Damien Harwood <1019243@student.sae.edu.au>
2020-12-03 23:02:52 +1100

    Beginnings  of AI enabler
    Not working yet

---
+++ ANURA/Assets/Scripts/AITrigger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AITrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider ai)
+    {
+        //TODO: Enable AI on toad
+        throw new NotImplementedException();
+        Debug.Log("Enable AI");
+    }
+
+    private void OnTriggerExit(Collider ai)
+    {
+        //TODO: Disable AI on toad
+        throw new NotImplementedException();
+        Debug.Log("Disable AI");
+    }
+
+    void EnemyTracker()
+    {
+        //TODO: Experiment with object tracker (Possibly not to be implemented)
+    }
+}
