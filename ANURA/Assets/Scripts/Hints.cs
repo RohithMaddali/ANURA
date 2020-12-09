84d0362da6d0929408e52d9220a7616fa2cfc138
Rohith Maddali <1015083@student.sae.edu.au>
2020-12-09 13:26:21 +1100

    Added hint system in loading screen

---
+++ ANURA/Assets/Scripts/Hints.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hints : MonoBehaviour
+{
+    public Text hints;
+    public LevelManager LV;
+    public string[] hint;
+    public int number, previousnum;
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LV = GameObject.FindGameObjectWithTag("LM").GetComponent<LevelManager>();
+        if (LV.levelWorks != true)
+        {
+            if (time == 0)
+            {
+                StartCoroutine(randomhint());
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LV.levelWorks != true)
+        {
+            if (time == 0)
+            {
+                StartCoroutine(randomhint());
+            }
+        }
+
+        else if (LV.levelWorks)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator randomhint()
+    {
+        number = Random.Range(0, hint.Length);
+        if (previousnum != number)
+        {
+            hints.text = hint[number];
+            previousnum = number;
+            time += Time.deltaTime;
+            yield return new WaitForSeconds(2f);
+            time = 0;
+        }
+    }
+}
