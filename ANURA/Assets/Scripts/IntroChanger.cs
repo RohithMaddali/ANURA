0bf1a0152be5d3933b210ad5a1ad91029f53baf7
Rohith Maddali <1015083@student.sae.edu.au>
2020-12-10 17:14:07 +1100

    Added cutscene tranistion

---
+++ ANURA/Assets/Scripts/IntroChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class IntroChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(end());
+    }
+    
+    IEnumerator end()
+    {
+        yield return new WaitForSeconds(18f);
+        SceneManager.LoadScene(1);
+    }
+}
